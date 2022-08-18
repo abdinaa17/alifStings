46e0a8b1a2931b43bbbd86c419ccca454c5d5e6a
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2022-08-17 23:24:10 -0400

    Navbar responsive design started but not completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
   return (
     <header>
       <nav className="container">
-        <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center nav">
             <div className="nav__logo">
               <a href="/"><span><em>biz</em></span>Listings</a>
             </div>
             <div className="nav__items">
-              <ul className="d-flex mr">
+              <ul className="d-flex">
                 <li>
                   <a href="/listings">Listings</a>
                 </li>
@@ -31,12 +31,12 @@ const Navbar = () => {
                   <a href="/listings">Listings</a>
                 </li>
               </ul>
-              <div>
+              <div className="nav__btns">
               <Button variant="custom"><a href="" >Register</a></Button>
               <Button variant="light"><a href="" >Login</a></Button>
             </div>
             </div>
-            <div>
+            <div className="nav__toggle-btn">
               <BiMenuAltRight size={30} />
             </div>
         </div>
---
+++ src/data/mockDB.js
@@ -0,0 +1,15 @@
+export const mockDB = [
+    {
+    "_id":"1",
+    "title":"Collier, Goldner and Stanton",
+    "description":"Fusce consequat. Nulla nisl. Nunc nisl.",
+    "image":"http://dummyimage.com/115x100.png/cc0000/ffffff",
+    "address":"4396 Dovetail Road",
+    "city":"Draginje",
+    "owner":"Faina Westover",
+    "numReviews":14,
+    "rating":3,
+    "longitude":"19.7483781",
+    "latitude":"44.5209494"
+    }
+ ]
