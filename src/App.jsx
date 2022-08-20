87f44f0e66f0ba5a0914bd41310a11740ed950ec
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2022-08-20 18:27:37 -0400

    Restructured the Navbar and simplified it

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import {Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // Local Imports
 import './App.css'
+import Test from './components/test/Test'
 import Navbar from './components/Navbar/Navbar'
+
 
 
 function App() {
 
   return (
     <main>
-      <Navbar />
+     {/* <Test /> */}
+     <Navbar />
     </main>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,53 +3,39 @@ import {useState} from 'react'
 import { BiMenuAltRight, BiX } from "react-icons/bi";
 import { Button } from "react-bootstrap";
 
-// Local Imports
-import "../Navbar/Navbar.css"
+import '../Navbar/Navbar.css'
 
-const Navbar = () => {
-  const [mobileMenu, setMobileMenu] = useState(false)
-
-  const handleToggle = () => {
-    setMobileMenu(!mobileMenu)
-  }
+const Test = () => {
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const handleToggle = () => {
+        setMobileMenu(!mobileMenu)
+      }
   return (
     <header>
-      <nav className="container">
-        <div className="nav">
-            <div className="nav__logo">
-              <a href="/"><span><em>biz</em></span>Listings</a>
+            <nav className="container nav">
+                <div className="logo"><a href="/"><span><em>biz</em></span>Listings</a></div>
+           <div className={mobileMenu ? "nav__content": "nav__content show"}>
+            <div className="nav__links">
+            <a href="" > Home</a>
+            <a href="" > Home</a>
+            <a href="" > Home</a>
+            <a href="" > Home</a>
+            <a href="" > Home</a>
             </div>
-            <div className={mobileMenu ? "nav__items": "nav__items show"}>
-              <ul className="d-flex">
-                <li>
-                  <a href="/listings">Listings</a>
-                </li>
-                <li>
-                  <a href="/listings">About Us</a>
-                </li>
-                <li>
-                  <a href="/listings">Listings</a>
-                </li>
-                <li>
-                  <a href="/listings">Listings</a>
-                </li>
-                <li>
-                  <a href="/listings">Listings</a>
-                </li>
-              </ul>
-              <div className="nav__btns">
-              <Button variant="custom"><a href="" >Register</a></Button>
+            
+            <div className='nav__btns'>
+            <Button variant="custom" style={{marginRight: '1rem'}}><a href="" >Register</a></Button>
               <Button variant="light"><a href="" >Login</a></Button>
             </div>
-            </div>
-            <div className="nav__toggle-btn">
-              {mobileMenu ? <BiX size={30} onClick={handleToggle} />: <BiMenuAltRight size={30} onClick={handleToggle} />}
-            </div>
-        </div>
-    </nav>
-    </header>
-    
+          </div>
+
+            <div className="nav__bars">
+            {mobileMenu ? <BiX size={30} onClick={handleToggle} />: <BiMenuAltRight size={30} onClick={handleToggle} />}
+          </div>
+            </nav>
+            
+        </header>
   )
 }
 
-export default Navbar
+export default Test
