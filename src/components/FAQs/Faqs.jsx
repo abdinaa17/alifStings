d71c299d96693c974919126254b1afd5e85fd158
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2022-09-27 22:46:07 -0400

    Styling done for the footer and responsiveness included

--- src/components/FAQs/Faqs.jsx
+++ src/components/FAQs/Faqs.jsx
@@ -7,7 +7,7 @@ const Faqs = () => {
        <h2 className="text-uppercase text-center my-5">frequently asked questions</h2>
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
-        <Accordion.Header>FAQ1</Accordion.Header>
+        <Accordion.Header>How Do I List My Businsess</Accordion.Header>
         <Accordion.Body>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -19,7 +19,55 @@ const Faqs = () => {
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>FAQ2</Accordion.Header>
+        <Accordion.Header>Can I Switch My Plan</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>How Does The Search Work</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header>Can I Earn From My directory</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="4">
+        <Accordion.Header>What is your 14 days money back guarantee?</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="5">
+        <Accordion.Header>Do You Provide Advertising</Accordion.Header>
         <Accordion.Body>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ import '../Footer/Footer.css'
 const Footer = () => {
   return (
     <footer className='footer mt-3'>
-        <Row className='container p-3'>
+      <Row className='container p-3'>
             <Col md={6} lg={3} className='flex-column mt-3'>
             
             <a href=""><em>alif</em>Stings</a>
@@ -22,43 +22,65 @@ const Footer = () => {
               <p><a href="tel:+13115552368">(614) 555-2368</a></p>
             </Col>
 
-            <Col md={6} lg={3}>
-              Find Us On our Socials
-              <Row className='flex-column mt-3 '>
-                <Col>
-                <FaTwitter size={24} />
+            <Col md={6} lg={3} className="mt-4">
+            <h6>Find Us On our Socials</h6>
+              <Row className='flex-md-column mt-3 align-items-center w-50'>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                     <FaTwitter size={24} />
+                  </Link>
                 </Col>
-                <Col>
-                  <FaLinkedin size={24}/>
+                <Col className="my-1">
+                <Link  to="/coming-soon">
+                     <FaLinkedin size={24} />
+                  </Link>
                 </Col>
-                <Col>
-                  <FaFacebook size={24}/>
+                <Col className="my-1">
+                <Link  to="/coming-soon">
+                     <FaFacebook size={24} />
+                  </Link>
                 </Col>
-                <Col>
-                  <FaYoutube size={24}/>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                     <FaYoutube size={24} />
+                  </Link>
                 </Col>
               </Row>
             </Col>
+
             <Col md={6} lg={3}>
-              <ul>
-                <li>
-                  <Link to="/coming-soon">Services</Link>
-                </li>
-                <li>
-                  <Link to="/coming-soon">Careers</Link>
-                </li>
-                <li>
-                  <Link to="/coming-soon">Blog</Link>
-                </li>
-                <li>
-                  <Link to="/coming-soon">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link to="/coming-soon">Terms and Conditions</Link>
-                </li>
-              </ul>
+              <Row className='flex-md-column mt-3 align-items-center'>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                     Services
+                  </Link>
+                </Col>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                    Careers
+                  </Link>
+                </Col>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                Blog
+                  </Link>
+                </Col>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                Privacy Policy
+                  </Link>
+                </Col>
+                <Col className="my-1">
+                <Link to="/coming-soon">
+                Terms and Conditions
+                  </Link>
+                </Col>
+                
+                
+              </Row>
             </Col>
         </Row>
+        
 
     </footer>
   )
