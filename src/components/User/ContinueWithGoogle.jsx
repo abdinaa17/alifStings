7c9b24c734f86a22a1cef865ec3fe8c969eb5c76
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2022-11-24 10:48:49 -0500

    Added the google login functionality.

---
+++ src/components/User/ContinueWithGoogle.jsx
@@ -0,0 +1,44 @@
+// Global Imports
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getDoc, setDoc, doc, serverTimestamp } from "firebase/firestore";
+import { Button } from "react-bootstrap";
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+
+// Local Imports
+import { auth, db } from "../../config/firebase";
+
+const ContinueWithGoogle = () => {
+  const navigate = useNavigate();
+  const loginWithGoogle = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Register user if they don't extist
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/dashoard");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <Button
+      onClick={loginWithGoogle}
+      variant="custom"
+      className="icon-text mx-auto w-75 mb-3"
+    >
+      <FcGoogle size={24} className="me-2" /> Continue with Google
+    </Button>
+  );
+};
+
+export default ContinueWithGoogle;
