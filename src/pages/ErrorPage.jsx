73731b386a644633cc663564ada024491e7d9338
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2022-12-08 15:01:15 -0500

    Restructured the 404 page to make it look better.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -6,15 +6,17 @@ import notFound from "../assets/images/not-found.svg";
 
 const Error = () => {
   return (
-    <section className="container p-5">
+    <section className="container p-5 vh-100">
       <Row>
-        <Col>
+        <Col md={4}>
           <h1>Oops!</h1>
           <h2>404 - Page not found</h2>
-          <Link to="/">
+          <Link to="/" className="mt-2">
             <Button variant="dark">Back To Home....</Button>
           </Link>
-          <img src={notFound} alt="Not Found" />
+        </Col>
+        <Col md={8}>
+          <img src={notFound} alt="Not Found" className="w-100" />
         </Col>
       </Row>
     </section>
