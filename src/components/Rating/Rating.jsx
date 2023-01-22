0e12c1c9cd7995c71f75ccf610240538b38eef61
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2023-01-21 20:49:38 -0500

    FIxed 5 star rating bug for the rating component.

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -9,35 +9,35 @@ const Rating = ({ rating }) => {
         <span className="text-warning">
           {rating >= 1 ? (
             <BsStarFill />
-          ) : rating <= 0.5 ? (
+          ) : rating >= 0.5 ? (
             <BsStarHalf />
           ) : (
             <BsStar />
           )}
           {rating >= 2 ? (
             <BsStarFill />
-          ) : rating <= 1.5 ? (
+          ) : rating >= 1.5 ? (
             <BsStarHalf />
           ) : (
             <BsStar />
           )}
           {rating >= 3 ? (
             <BsStarFill />
-          ) : rating <= 2.5 ? (
+          ) : rating >= 2.5 ? (
             <BsStarHalf />
           ) : (
             <BsStar />
           )}
           {rating >= 4 ? (
             <BsStarFill />
-          ) : rating <= 3.5 ? (
+          ) : rating >= 3.5 ? (
             <BsStarHalf />
           ) : (
             <BsStar />
           )}
-          {rating === 5 ? (
+          {rating >= 5 ? (
             <BsStarFill />
-          ) : rating <= 4.5 ? (
+          ) : rating >= 4.5 ? (
             <BsStarHalf />
           ) : (
             <BsStar />
