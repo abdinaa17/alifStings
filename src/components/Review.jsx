0e5a20791e44f53bf5d4a077b5351bfcbd39cde4
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2023-02-08 01:44:21 -0500

    Put all the review code in the review component.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,7 +1,93 @@
 import React from "react";
+import { Col, Row, Form, Modal, Button } from "react-bootstrap";
+import Message from "./Message/Message";
 
-const Review = () => {
-  return <div>Review</div>;
+const Review = ({
+  listing,
+  handleReview,
+  user,
+  isModal,
+  setIsModal,
+  handleChange,
+  review,
+  error,
+}) => {
+  return (
+    <Row className="my-4">
+      <Col md={6}>
+        {user ? (
+          <>
+            <Button onClick={() => setIsModal(true)}>Write a review</Button>
+            <Modal show={isModal} onHide={() => setIsModal(false)} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>{listing.title}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                {" "}
+                <Form onSubmit={handleReview} className="form__section">
+                  <Form.Group className="mb-3">
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control
+                      as="select"
+                      name="rating"
+                      value={review.rating}
+                      onChange={handleChange}
+                    >
+                      <option>Select...</option>
+                      <option value="1">1-Poor</option>
+                      <option value="2">2-Fair</option>
+                      <option value="3">3-Good</option>
+                      <option value="4">4-Very good</option>
+                      <option value="5">5-Excellent</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Comment</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      name="comment"
+                      value={review.comment}
+                      onChange={handleChange}
+                      style={{ height: "150px" }}
+                    />
+                  </Form.Group>
+                  <Button type="submit" className="w-100 mt-3">
+                    Post review
+                  </Button>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                {error && (
+                  <Message
+                    variant="danger"
+                    className="capitalize-first mx-auto"
+                  >
+                    {error}
+                  </Message>
+                )}
+              </Modal.Footer>
+            </Modal>
+          </>
+        ) : (
+          <Message>
+            <p>
+              Please{" "}
+              <Link
+                to="/login"
+                style={{
+                  color: "inherit",
+                  borderBottom: "2px solid #000",
+                }}
+              >
+                Log In
+              </Link>{" "}
+              to leave a review
+            </p>
+          </Message>
+        )}
+      </Col>
+    </Row>
+  );
 };
 
 export default Review;
